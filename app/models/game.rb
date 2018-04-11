ec062a21641f852a16f56b6fc0d888e8c97961a1
Nikko <nikkovichert@gmail.com>
2018-04-11 07:15:16 +0000

    Made a few more changes to DB structure after week 1 meeting. Should be complete now

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
+  belongs_to :white_player, :class_name => 'User'
+  belongs_to :black_player, :class_name => 'User'
   belongs_to :user
   has_many :pieces
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :created_games, :class_name => 'Game' 
+  has_many :joined_games, :class_name => 'Game' 
   has_many :games
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
