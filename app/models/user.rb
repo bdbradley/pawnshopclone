4ddbf15491ba3070ddf92f31e84a0b0116312ff8
Nikko <nikkovichert@gmail.com>
2018-04-11 07:42:56 +0000

    Last take on DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  has_many :created_games, :class_name => 'Game' 
-  has_many :joined_games, :class_name => 'Game' 
-  has_many :games
+  has_many :created_games, :class_name => 'Game', foreign_key: "owner_id" 
+  has_many :joined_games, :class_name => 'Game',  foreign_key: "opponent_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
