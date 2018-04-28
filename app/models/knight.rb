780591b35460cb13adf3d65d8ba0352beb727b1a
Barry Bradley <bbradley13@gmail.com>
2018-04-28 17:38:40 +0000

    Valid moves for knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,12 @@
 class Knight < Piece
+
+    def valid_move?(new_x, new_y)
+      x_dif = (new_x.to_i - x_position).abs
+      y_dif = (new_y.to_i - y_position).abs
+      #knight moves one up and two over or two up and one over
+      (x_dif == 1 && y_dif == 2) || (x_dif == 2 && y_dif == 1)
+    end
+  
 end
+
+ 
