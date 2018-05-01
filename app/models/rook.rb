856db372c473290b901b2442d9ae0cb9b34579dd
Nikko <nikkovichert@gmail.com>
2018-05-01 01:32:38 +0000

    messing with rook, trying to fix repo not lining up, lost some code somewhere

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,2 +1,10 @@
 class Rook < Piece
+  def valid_move?(new_x, new_y)
+    unless is_obstructed?(new_x, new_y)
+      return true if (new_x.to_i - x_position).abs == (new_y.to_i - y_position).abs || 
+      new_x.to_i == x_position || 
+      new_y.to_i == y_position
+    end
+    false
+  end
 end
