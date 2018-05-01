9fc4febb4dd056ac10fdb72c207811e44b636db1
Nikko <nikkovichert@gmail.com>
2018-05-01 02:40:39 +0000

    Rook valid_move done

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,6 +1,8 @@
 class Rook < Piece
-  def move_rook(new_x, new_y)
-    if valid_move?(new_x, new_y) 
-      self.move_to!(new_x, new_y)
+  def valid_move?(new_x, new_y)
+    square_occupied?(new_x, new_y) && self.color != game.get_piece_at_coor(new_x, new_y).color
+    !is_obstructed?(new_x, new_y)
+    (y_position == new_y) && (x_position != new_x) || (x_position == new_x) && (y_position !=new_y)
+    !off_board?(new_x new_y)
   end
 end
