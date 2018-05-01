3ffa652605c329dcf26ba2479e841543bb88af68
Barry Bradley <bbradley13@gmail.com>
2018-05-01 03:35:45 +0000

    Changed field name from moved to piece_moved

--- db/migrate/20180501033402_add_piece_moved_to_pieces.rb
+++ db/migrate/20180501033402_add_piece_moved_to_pieces.rb
@@ -1,5 +1,5 @@
 class AddPieceMovedToPieces < ActiveRecord::Migration[5.0]
   def change
-    add_column :pieces, :moved, :boolean
+    add_column :pieces, :piece_moved, :boolean
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180501030748) do
+ActiveRecord::Schema.define(version: 20180501033402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,11 @@ ActiveRecord::Schema.define(version: 20180501030748) do
     t.integer  "y_position"
     t.string   "color"
     t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "game_id"
     t.boolean  "is_white"
-    t.boolean  "moved"
+    t.boolean  "piece_moved"
     t.index ["game_id"], name: "index_pieces_on_game_id", using: :btree
   end
 
