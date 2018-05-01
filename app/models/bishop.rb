3c75efdf71e5cc08566091ed600b81e5a14b44fe
Nikko <nikkovichert@gmail.com>
2018-05-01 23:47:13 +0000

    fixed lack of &&s in bishop

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -2,10 +2,10 @@ class Bishop < Piece
   def valid_move?(new_x, new_y)
     x1 = self.x_position
     y1 = self.y_position
-    own_piece_at_destination?(new_x, new_y)
-    !is_obstructed?(new_x, new_y)
-    x_position != new_x && y_position != new_y
-    (x1 + new_x).abs === (y1 + new_y).abs
+    own_piece_at_destination?(new_x, new_y) &&
+    !is_obstructed?(new_x, new_y) &&
+    (x_position != new_x && y_position != new_y) &&
+    ((x1 + new_x).abs === (y1 + new_y).abs) &&
     !off_board?(new_x new_y)
   end
 end
