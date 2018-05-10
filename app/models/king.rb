09b46aef9575b65d90f24ea50b4c42d42d4d629a
ltsinh <linhtsinh@gmail.com>
2018-05-09 22:01:59 -0400

    Add_Checkmate_method

--- app/models/king.rb
+++ app/models/king.rb
@@ -3,20 +3,31 @@ class King < Piece
 
 	def valid_move?(new_x, new_y)
 		#return false if is_obstructed?(new_x.to_i, new_y.to_i)
-  	return true if 
+  	return true if
   		x_difference = (new_x.to_i - x_position).abs
     	y_difference = (new_y.to_i - y_position).abs
     	(x_difference <= 1) && (y_difference <= 1)
   end
   	false
-end 
-  #still need to account for invalid move if King moves into check
+end
+
+	# determine if king can move himself out of check
+ def can_move_out_of_check?
+	 start_x = x_position
+	 start_y = y_position
+	 success = false
+	 ((x_position - 1)..(x_position + 1)).each do |x|
+		 ((y_position - 1)..(y_position + 1)).each do |y|
+			 update_attributes(x_position: x, y_position: y) if valid_move?(x, y)
+			 # if game.check?(color) comes up false,
+			 # even once, assign  true
+			 success = true unless game.check?(color)
+			 # reset any attempted moves
+			 update_attributes(x_position: start_x, y_position: start_y)
+		 end
+	 end
+	 success
+ end
 
 
 KING = 'King'.freeze
-
-
-    
-    	
-    
-    	
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -27,7 +27,7 @@ class Piece < ApplicationRecord
         capture_piece!(occupying_piece)
       end
       update(x_position: new_x, y_position: new_y)
-      
+
     end
   end
 
@@ -36,9 +36,9 @@ class Piece < ApplicationRecord
     return true if off_board?(new_x, new_y)
     return true if straight_obstruction?(new_x, new_y)
     false
-  end 
+  end
 
-  #covers vertical, horizontal, and diagonal moves 
+  #covers vertical, horizontal, and diagonal moves
   def straight_move?(new_x, new_y)
     (new_x.to_i - x_position.to_i != 0 || new_y.to_i - y_position.to_i != 0) &&
       (new_x == x_position || new_y == y_position || (new_x.to_i - x_position.to_i).abs == (new_y.to_i - y_position.to_i).abs)
@@ -64,19 +64,19 @@ class Piece < ApplicationRecord
   def straight_obstruction_array(new_x, new_y)
     return nil unless straight_move?(new_x, new_y)
     x_values = if x_position.to_i < new_x.to_i
-                 (x_position.to_i..new_x.to_i).to_a 
+                 (x_position.to_i..new_x.to_i).to_a
                elsif x_position.to_i > new_x.to_i
                  x_position.to_i.downto(new_x.to_i).to_a
-               else 
+               else
                  range_y = (new_y.to_i - y_position.to_i).abs
-                 Array.new(range_y + 1, new_x.to_i) 
+                 Array.new(range_y + 1, new_x.to_i)
                end
 
     y_values = if y_position.to_i < new_y.to_i
-                 (y_position.to_i..new_y.to_i).to_a 
+                 (y_position.to_i..new_y.to_i).to_a
                elsif y_position.to_i > new_y
                  y_position.to_i.downto(new_y.to_i).to_a
-               else 
+               else
                  Array.new(range_x + 1, new_y.to_i)
                end
     coordinates_array(x_values, y_values)
@@ -109,11 +109,54 @@ class Piece < ApplicationRecord
     update(has_moved: true)
   end
 
+#Method to determine checkmate
 
+  # determine if a state of checkmate has occurred
+  def checkmate?(color)
+    checked_king = pieces.find_by(type: 'King', color: color)
+
+    # make sure color is in check and get @piece_causing_check
+    return false unless check?(color)
+
+    # see if another piece can capture checking piece
+    return false if @piece_causing_check.capture_piece?
+
+    # see if king can get himself out of check
+    return false if checked_king.can_move_out_of_check?
+
+    # # see if another piece can block check
+    return false if @piece_causing_check.can_be_blocked?(checked_king)
+    true
+  end
+
+  # determines if color is in check
+  def check?(color)
+    king = pieces.find_by(type: 'King', color: color)
+    opponents = pieces_remaining(!color)
+
+    opponents.each do |piece|
+      if piece.valid_move?(king.x_position, king.y_position)
+        @piece_causing_check = piece
+        return true
+      end
+    end
+    false
+  end
+
+  # method to determine if an opposing piece can block check
+  # this method is called to determine checkmate.
+  def can_be_blocked?(king)
+    # get all possible squares that could be used to obstruct check
+    obstruction_array = obstructed_squares(king.x_position, king.y_position)
+    opponents = game.pieces_remaining(!color)
+    # for each opponent, iterate through all squares that could obstruct
+    opponents.each do |opponent|
+      next if opponent.type == 'King'
+      obstruction_array.each do |square|
+        # return true if we find even one way to obstruct check
+        return true if opponent.valid_move?(square[0], square[1])
+      end
+    end
+    false
+  end
 end
-
-  
-      
-        
-
-
