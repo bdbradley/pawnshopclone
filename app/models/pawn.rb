dd1dd07041fbc75ab1fd92abc9098d7522625c1f
ltsinh <linhtsinh@gmail.com>
2018-05-09 22:31:09 -0400

    Add_obstructed_squares

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,75 +1,79 @@
 class Pawn < Piece
-
-
-  	def valid_move?(new_x, new_y)
-      return false if still_in_starting_square?(new_x.to_i, new_y.to_i)
-      return false if backwards_move?(new_y.to_i)
-      return false if sideways_move?(new_x.to_i, new_y.to_i)
-      #return false if is_obstructed?(new_x.to_i, new_y.to_i)
-      return false if square_occupied?(new_x.to_i, new_y.to_i) && forwards_straight_move?(new_x.to_i, new_y.to_i)
-      return true if pawn_capture?(new_x.to_i, new_y.to_i)
-      if move_two_squares_ok?(new_x.to_i, new_y.to_i) && !square_occupied?(new_x.to_i, new_y.to_i)
+  def valid_move?(new_x, new_y)
+    return false if still_in_starting_square?(new_x.to_i, new_y.to_i)
+    return false if backwards_move?(new_y.to_i)
+    return false if sideways_move?(new_x.to_i, new_y.to_i)
+    # return false if is_obstructed?(new_x.to_i, new_y.to_i)
+    return false if square_occupied?(new_x.to_i, new_y.to_i) && forwards_straight_move?(new_x.to_i, new_y.to_i)
+    return true if pawn_capture?(new_x.to_i, new_y.to_i)
+    if move_two_squares_ok?(new_x.to_i, new_y.to_i) && !square_occupied?(new_x.to_i, new_y.to_i)
       update(turn_pawn_moved_twice: game.move_number + 1) if moving_two_squares?(new_x.to_i, new_y.to_i)
-        return true
-      end
-      false
+      return true
     end
-      
+    false
+   end
 
-    def can_attack_square?(new_x, new_y)
-      x_difference = (new_x.to_i - x_position.to_i).abs
-      y_difference = (new_y.to_i - y_position.to_i).abs
-        return true if x_difference == 1 && y_difference == 1
-      false
-    end
+  def can_attack_square?(new_x, new_y)
+    x_difference = (new_x.to_i - x_position.to_i).abs
+    y_difference = (new_y.to_i - y_position.to_i).abs
+    return true if x_difference == 1 && y_difference == 1
+    false
+  end
 
-    private 
+  private
 
-    def still_in_starting_square?(new_x, new_y)
+  def obstructed_squares(new_x, new_y)
+    # check if a white 2 square move with obstruction
+    return [[new_x, 2]] if y_position == 1 && new_y == 3
+    # check if a black 2 square move with obstruction
+    return [[new_x, 5]] if y_position == 6 && new_y == 4
+
+    []
+  end
+
+  def still_in_starting_square?(new_x, new_y)
     x_position == new_x && y_position == new_y
-    end
-      
-  
-    def pawn_capture?(new_x, new_y)
-      x_difference = (new_x.to_i - x_position.to_i).abs
-      y_difference = (new_y.to_i - y_position.to_i).abs
-      piece_to_capture   = Piece.exists?(x_position: new_x, y_position: new_y, is_white: !is_white, game: game)
-      return true if piece_to_capture && x_difference == 1 && y_difference == 1
-      false
-    end
+  end
 
-    def forwards_straight_move?(new_x, new_y)
-      return false if new_x != x_position
-      return new_y > y_position if is_white
-      new_y < y_position
-    end
+  def pawn_capture?(new_x, new_y)
+    x_difference = (new_x.to_i - x_position.to_i).abs
+    y_difference = (new_y.to_i - y_position.to_i).abs
+    piece_to_capture = Piece.exists?(x_position: new_x, y_position: new_y, is_white: !is_white, game: game)
+    return true if piece_to_capture && x_difference == 1 && y_difference == 1
+    false
+  end
 
-    def current_position?(new_x, new_y)
+  def forwards_straight_move?(new_x, new_y)
+    return false if new_x != x_position
+    return new_y > y_position if is_white
+    new_y < y_position
+  end
+
+  def current_position?(new_x, new_y)
     x_position == new_x && y_position == new_y
-    end
+  end
 
-    def sideways_move?(new_x, new_y)
-      x_difference = (x_position - new_x).abs
-      x_difference != 0 && new_y == y_position
-    end
+  def sideways_move?(new_x, new_y)
+    x_difference = (x_position - new_x).abs
+    x_difference != 0 && new_y == y_position
+  end
 
-    def backwards_move?(new_y)
-      return new_y < y_position if is_white
-      new_y > y_position
-    end
+  def backwards_move?(new_y)
+    return new_y < y_position if is_white
+    new_y > y_position
+  end
 
-    def move_two_squares_ok?(new_x, new_y)
-      x_difference = (new_x.to_i - x_position.to_i).abs
-      y_difference = (new_y.to_i - y_position.to_i).abs
-      if piece_moved?  
-        x_difference.zero? && y_difference == 1
-        #only allowed to move one space if pawn has already moved
-      else
-        x_difference.zero? && y_difference == 1 || x_difference.zero? && y_difference == 2
-        #allowed to move 1 or 2 spaces if pawn has not moved yet
-      end
+  def move_two_squares_ok?(new_x, new_y)
+    x_difference = (new_x.to_i - x_position.to_i).abs
+    y_difference = (new_y.to_i - y_position.to_i).abs
+    if piece_moved?
+      x_difference.zero? && y_difference == 1
+      # only allowed to move one space if pawn has already moved
+    else
+      x_difference.zero? && y_difference == 1 || x_difference.zero? && y_difference == 2
+      # allowed to move 1 or 2 spaces if pawn has not moved yet
     end
-    
+  end
 end
 
 PAWN = 'Pawn'.freeze
--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,17 +1,12 @@
 class Rook < Piece
-  
+
   def valid_move?(new_x, new_y)
-    #return false if is_obstructed?(new_x.to_i, new_y.to_i)
+    # return false if is_obstructed?(new_x.to_i, new_y.to_i)
     return true if new_x.to_i == x_position || new_y.to_i == y_position
-  end 
-    false 
-    
-end 
+  end
+  false
+end
 
-
-
-    	
-    
-
-
-  
+  def obstructed_squares(new_x, new_y)
+    rectilinear_obstruction_array(new_x, new_y)
+  end
