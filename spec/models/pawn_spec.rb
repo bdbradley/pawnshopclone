c4fbcbfe216d07adcfd4d409e59748dc0265908f
ltsinh <linhtsinh@gmail.com>
2018-05-19 12:22:37 -0400

    Update_Spec

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -19,26 +19,3 @@ RSpec.describe Pawn, type: :model do
     expect(pawn.valid_move?(4, 4)).to eq true
   end
 end
-
-describe 'Pawn Promotion' do
-  context 'promotable method' do
-    it 'Should show that a pawn is promotable' do
-      board = create(:game)
-
-
-      pawn = Pawn.create(color: 'WHITE', x_position: 0, y_position: 7, game_id: board.id)
-      board.pieces << pawn
-
-      expect(pawn.promotable?(0, 7)).to eq(true)
-    end
-
-    it 'Should show that a pawn is not promotable' do
-      board = create(:game)
-
-      pawn = Pawn.create(color: 'WHITE', x_position: 0, y_position: 6, game_id: board.id)
-      board.pieces << pawn
-
-      expect(pawn.promotable?(0, 6)).to eq(false)
-    end
-  end
-end
