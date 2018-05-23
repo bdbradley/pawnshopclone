a828c4332bfbfbcbaf3a9462d50c4e153cb7fbff
Barry Bradley <bbradley13@gmail.com>
2018-05-23 03:01:56 +0000

    Test for off_board method

---
+++ spec/models/piece_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Piece, type: :model do
+
+describe '#off_board?' do
+    let(:piece) { FactoryBot.create :piece, x_position: 1, y_position: 9, is_white: true }
+      
+    it 'returns true if piece off_board' do
+      expect(piece.off_board?(1, 9)).to eq true
+    end
+  end
+end
